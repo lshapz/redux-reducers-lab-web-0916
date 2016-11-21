2d3143c8abe9f68b4ff3f25730a2b9ec81293838
lshapz <laurashapiro01@gmail.com>
2016-11-21 17:59:04 -0500

    Done.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,10 @@
-export default function ingredientReducer() {
-
+export default function ingredientReducer(state = [], action) {
+switch (action.type) {
+  case 'ADD_INGREDIENT':
+    return [...state, action.payload];
+  case 'REMOVE_INGREDIENT':
+    return state = state.slice(0, action.payload).concat(state.slice((action.payload)+1, state.length));
+  default:
+    return state;
+}
 }
